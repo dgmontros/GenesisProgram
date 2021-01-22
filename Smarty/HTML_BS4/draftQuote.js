edddd8a705fc6d45b71897b9bfb26c0e8b0471b9
Dennis Mont'Ros <montros@gmail.com>
2021-01-21 19:52:05 -0500

    Updates as of 21 January 2021

--- Smarty/HTML_BS4/draftQuote.js
+++ Smarty/HTML_BS4/draftQuote.js
@@ -1,146 +1,114 @@
 
+var selectBuilding = document.getElementById('selectBuilding');
+var residentialForm = document.getElementById('residentialForm');
+var commercialForm = document.getElementById('commercialForm');
+var corporateForm = document.getElementById('corporateForm');
+var hybridForm = document.getElementById('hybridForm');
 
+//selectBuilding.addEventListener("select", selectForm());
 
+	
+// function selectForm(selectBuilding) {
+	
+	
+	// console.log(document.getElementById('#selectBuilding').value === 'commercial')
+	//  console.log($('#selectBuilding').val());
+	// console.log(select.selectBuilding.hybrid);
+	
 
-function selectForm() {
+	selectBuilding.addEventListener('change', function(){
+
+		if (selectBuilding.value === 'residential') {
+
+			residentialForm.style.display = "block";
+			corporateForm.style.display = "none";
+			commercialForm.style.display = "none";
+			hybridForm.style.display = "none";
+
+		}
+
+		else if (selectBuilding.value === 'corporate') {
+
+			residentialForm.style.display = "none";
+			corporateForm.style.display = "block";
+			commercialForm.style.display = "none";
+			hybridForm.style.display = "none";
+
+	}
+	else if (selectBuilding.value === 'commercial') {
+
+		residentialForm.style.display = "none";
+		corporateForm.style.display = "none";
+		commercialForm.style.display = "block";
+		hybridForm.style.display = "none";
 
 }
 
+	else if (selectBuilding.value === 'hybrid') {
+
+		residentialForm.style.display = "none";
+		corporateForm.style.display = "none";
+		commercialForm.style.display = "none";
+		hybridForm.style.display = "block";
+
+}
+
+	});
+
+
+	
 
 
 
 
+	
+	 // 	if (selectBuilding(document.getElementById(residential)) 
+	// 	{
+	// 		console.log(selectForm) === (document.getElementById('residentialForm'));
+		
+		// /quote.getElementById(residentialOutput);
+		// }
+		// else if (selectBuilding.type === commercial) {
+		// 	quote.getElementById(commercialOutput);
+		// }
+		// else if (selectBuilding.type === corporate) {
+		// 	quote.getElementById(corporateOutput);
+		// }
+		// else if (selectBuilding.type === hybrid) {
+		// 	quote.getElementById(hybridOutput);
+		// }
 
-$(document).ready(function () {
-  $('.building_type').hide();
+
+//}
+
+
+
+
+// $(function() {
+//   var fields = $('#form1 :input').change(calculate)
+//   $("#form1 option").text(function(i, t) {
+//     if (this.value !== "0")
+//       return t + " - " + this.value
+//   }).first().change()
   
-  $('#selectMe').change(function () {
-      $('.building_type').hide();
-      $('#'+$(this).val()).show();
-  })
-});
+// function calculate() {
+//   var price = 0;
+//   var list = []
+//   fields.each(function() {
+//     price += +$(this).val();
+//     if ($(this).val() > 0)
+//       list.push($(this).find("option:selected").text())
+//   })
+//   $("#list").html(list.join("<br>"))
+//   $('#price').html(price.toFixed(2));
+// }
+// });
 
-var residentialForm = "residential-form"
-var commercialForm = "commercial-form"
-var corporateForm = "corporate-form"
-var hybridForm = "hybrid-form"
+// var selectBuilding = $('#selectBuilding');
+// var residential = selectBuilding.type(residential);
+// var residentialOuput = ["number-of-apartments", "number-of-floors", "number-of-basements"]
 
-
-
-/*
-
-selectBuilding.onchange = selectForm() {
-  if selectBuilding.value=residential {
-    quote.getElementById(residential-form)
-}
-  else if selectBuilding.value=commercial {
-    quote.getElementById(commercial-form)
-}
-  else if selectBuilding.value=corporate {
-    quote.getElementById(corporate-form)
-}
-  else if selectBuilding.value=hybrid {
-    quote.getElementById(hybrid-form)
-}
-
-selectForm();
-
-
-
-
-
-
-
-
-
-
-/*--BEGIN CODE FOR JQUERY CALCULATOR FORM>
-quoteFormElements: [
-	{  
-		type: "residential",
-		input fields: ["number-of-apartments", "number-of-floors", "number-of-basements"]
-	},
-{  
-		type: "commercial",
-		input fields: ["number-of-floors", "number-of-basements", "number-of-companies", "number-of-parking-spots", "number-of-elevators"]
-	},
-{  
-		type: "corporate",
-		input fields: ["number-of-floors", "number-of-basements", "number-of-parking-spots", "number-of-corporations", "maximum-occupancy"]
-	},
-{  
-		type: "hybrid",
-		input fields: ["number-of-floors", "number-of-basements", "number-of-companies","number-of-parking-spots", "maximum-occupancy", "business-hours"]
-	},
-
-
-<!-- <div id= "number-of-apartments" ...>
-	<label>N. of Apartments </label>
-	<input  ...>
-	</div>
-
-	<div id= "number-of-floors" ...>
-	<label>N. of Floors </label>
-	<input  ...>
-	</div>
-
-	<div id= "number-of-companies" ...>
-	<label>N. of Companies </label>
-	<input  ...>
-	</div>
-
-	<div id= "number-of-corporations" ...>
-	<label>N. of Corporations </label>
-	<input  ...>
-	</div>
-
-	<div id= "number-of-basements" ...>
-	<label>N. of Basements </label>
-	<input  ...>
-	</div>
-
-	<div id= "number-of-parking-spots" ...>
-	<label>N. of Parking Spots </label>
-	<input  ...>
-	</div>
-
-	<div id= "number-of-apartments" ...>
-	<label>N. of Apartments </label>
-	<input  ...>
-	</div>
-
-	<div id= "number-of-elevators" ...>
-	<label>N. of Elevators </label>
-	<input  ...>
-  </div>
-  
-	<div id= "maximum-occupancy" ...>
-	<label>Maximum Occupancy</label>
-	<input  ...>
-	</div>
-
-*/
-
-
-
-
-
-$(function() {
-  var fields = $('#form1 :input').change(calculate)
-  $("#form1 option").text(function(i, t) {
-    if (this.value !== "0")
-      return t + " - " + this.value
-  }).first().change()
-  
-function calculate() {
-  var price = 0;
-  var list = []
-  fields.each(function() {
-    price += +$(this).val();
-    if ($(this).val() > 0)
-      list.push($(this).find("option:selected").text())
-  })
-  $("#list").html(list.join("<br>"))
-  $('#price').html(price.toFixed(2));
-}
-})
+// var commercial = selectBuilding.type(commercial)
+// var commercialOutput = ["number-of-floors", "number-of-basements", "number-of-companies", "number-of-parking-spots", "number-of-elevators"]
+//var selectBuildingType = selectBuilding.options[selectBuilding.selectedIndex].innerHTML;
+	//var selectForm = selectBuilding.value;
